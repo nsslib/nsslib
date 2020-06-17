c7140ba817ae71728ccfc56c1dc0389b6d729b52
ozan <ol1915101012@isparta.edu.tr>
2020-06-17 23:47:09 +0300

    Project initialization

---
+++ src/impl.c
@@ -0,0 +1,22 @@
+#include "./impl.h"
+
+napi_value 
+initValue(napi_env env, napi_callback_info info) 
+{
+
+    return NULL;
+}
+
+napi_value 
+insertSlot(napi_env env, napi_callback_info info) 
+{
+
+    return NULL;
+}
+
+napi_value 
+emitSignal(napi_env env, napi_callback_info info) 
+{
+
+    return NULL;
+}
---
+++ src/impl.h
@@ -0,0 +1,27 @@
+#ifndef __IMPLEMENTATIONS__
+#include <node_api.h>
+
+typedef struct _signal signal;
+typedef struct _slot slot;
+
+struct _signal {
+    napi_value val;
+    slot *slots;
+};
+
+struct _slot {
+    napi_value fn;
+};
+
+
+napi_value 
+initValue(napi_env env, napi_callback_info info);
+
+napi_value 
+insertSlot(napi_env env, napi_callback_info info);
+
+napi_value 
+emitSignal(napi_env env, napi_callback_info info);
+
+
+#endif // __IMPLEMENTATIONS__
